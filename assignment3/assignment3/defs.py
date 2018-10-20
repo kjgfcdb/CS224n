9ffc34d26faf63af188dc1050780399fb604a4e1
kjgfcdb <kjgfcdb@126.com>
2018-10-20 13:15:45 +0800

    assignment 3 finished

---
+++ assignment3/assignment3/defs.py
@@ -0,0 +1,19 @@
+"""
+Common definitions for NER
+"""
+
+from util import one_hot
+
+LBLS = [
+    "PER",
+    "ORG",
+    "LOC",
+    "MISC",
+    "O",
+    ]
+NONE = "O"
+LMAP = {k: one_hot(5,i) for i, k in enumerate(LBLS)}
+NUM = "NNNUMMM"
+UNK = "UUUNKKK"
+
+EMBED_SIZE = 50
